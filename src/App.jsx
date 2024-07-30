837548d6d8b76766904913c0120f5a13d68462eb
Ahmad Badar <ahmadbadar145@gmail.com>
2024-07-30 10:44:27 +0700

    Menambah beberapa file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Logo from './assets/Logo KPU.png';
+
 function App() {
   return (
     <div>
       <h1>Hello World!</h1>
       <p>Assalamualaikum barudak</p>
+      <img src={Logo} alt="Logo KPU" />
     </div>
   );
 }
