39788ef925b8b0a23f58bab07f835f32155b9dfe
Ahmad Badar <ahmadbadar145@gmail.com>
2024-07-31 11:42:22 +0700

    Memperbaiki responsive

--- src/pages/Admin/DashboardAdmin.jsx
+++ src/pages/Admin/DashboardAdmin.jsx
@@ -12,6 +12,10 @@ const DashboardAdmin = () => {
     setMenuOpen(!menuOpen);
   };
 
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   const toggleDropdown = (dropdown) => {
     setActiveDropdown(activeDropdown === dropdown ? null : dropdown);
   };
@@ -59,12 +63,10 @@ const DashboardAdmin = () => {
                   </ul>
                 )}
               </li>
-              <li className="relative">
-                <button
-                  className="block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-black md:p-0"
-                >
+              <li>
+                <Link to="/" className="block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-black md:p-0">
                   Sekretaris
-                </button>
+                </Link>
               </li>
               <li className="relative">
                 <button
@@ -126,20 +128,102 @@ const DashboardAdmin = () => {
         </div>
       </nav>
 
-      <section class="bg-center bg-no-repeat bg-gray-700 bg-blend-multiply" style={{ backgroundImage: `url(${BackgroundImage})` }}>
-          <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-              <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-5xl">Pengelolaan Pegawai KPU Kota Cimahi</h1>
-              <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Solusi Terbaik untuk Pengelolaan dan Pemantauan Data Pegawai KPU</p>
-              <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-                  <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                      Get started
-                      <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                      </svg>
-                  </a>
-              </div>
+      <section className="bg-center bg-no-repeat bg-gray-700 bg-blend-multiply" style={{ backgroundImage: `url(${BackgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+        <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-5xl">Pengelolaan Pegawai KPU Kota Cimahi</h1>
+          <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Solusi Terbaik untuk Pengelolaan dan Pemantauan Data Pegawai KPU</p>
+          <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+            <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+              Get started
+              <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+              </svg>
+            </a>
           </div>
+        </div>
       </section>
+
+      {/* Mobile menu */}
+      {menuOpen && (
+        <div className="md:hidden fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-75 z-50">
+          <div className="flex flex-col items-center pt-10">
+            <Link to="/DashboardAdmin" onClick={closeMenu} className="text-white text-2xl mb-6">≡</Link>
+            <ul className="flex flex-col items-center space-y-4">
+              <li>
+                <button
+                  onClick={() => toggleDropdown('divisi')}
+                  className="text-white text-lg"
+                >
+                  Divisi
+                </button>
+                {activeDropdown === 'divisi' && (
+                  <ul className="bg-white text-black rounded shadow-lg mt-2 w-48">
+                    <li>
+                      <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm" onClick={closeMenu}>Divisi Keuangan, Umum, Rumah Tangga, dan Logistik</Link>
+                    </li>
+                    <li>
+                      <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm" onClick={closeMenu}>Divisi Teknik Penyelenggaraan</Link>
+                    </li>
+                    <li>
+                      <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm" onClick={closeMenu}>Divisi Perencanaan, Data, & Informasi</Link>
+                    </li>
+                    <li>
+                      <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm" onClick={closeMenu}>Divisi Hukum dan Pengawasan</Link>
+                    </li>
+                    <li>
+                      <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm" onClick={closeMenu}>Divisi Sosialisasi, Pendidikan Pemilih, Parmas, & SDM</Link>
+                    </li>
+                  </ul>
+                )}
+              </li>
+              <li>
+                <Link to="/" className="block py-2 px-3 text-white text-lg" onClick={closeMenu}>
+                  Sekretaris
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={() => toggleDropdown('subBagian')}
+                  className="text-white text-lg"
+                >
+                  Sub Bagian
+                </button>
+                {activeDropdown === 'subBagian' && (
+                  <ul className="bg-white text-black rounded shadow-lg mt-2 w-48">
+                    <li>
+                      <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm" onClick={closeMenu}>Sub Bagian Teknis Penyelenggaraan Pemilu, Partisipasi, & Hupmas</Link>
+                    </li>
+                    <li>
+                      <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm" onClick={closeMenu}>Sub Bagian Perencanaan, Data & Informasi</Link>
+                    </li>
+                    <li>
+                      <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm" onClick={closeMenu}>Sub Bagian Hukum & SDM</Link>
+                    </li>
+                    <li>
+                      <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm" onClick={closeMenu}>Sub Bagian Keuangan, Umum, & Logistik</Link>
+                    </li>
+                  </ul>
+                )}
+              </li>
+              <li>
+                <button
+                  onClick={toggleAdminDropdown}
+                  className="text-white text-lg"
+                >
+                  Hallo, Admin!
+                </button>
+                {adminDropdownOpen && (
+                  <ul className="bg-white text-black rounded shadow-lg mt-2 w-30">
+                    <li>
+                      <Link to="/" className="block px-4 py-2 hover:bg-gray-200 rounded text-center" onClick={closeMenu}>Logout</Link>
+                    </li>
+                  </ul>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
