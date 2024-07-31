45614ea2972407bf26edac53a6c9d8f9c31bbcb2
dittoRW <147579048+dittoRW@users.noreply.github.com>
2024-07-31 11:21:33 +0700

    Menambahkan Jumbotron

--- src/pages/Admin/DashboardAdmin.jsx
+++ src/pages/Admin/DashboardAdmin.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/Logo KPU.png';
+import BackgroundImage from '../../assets/bg-KPU.png';
 
 const DashboardAdmin = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -20,109 +21,126 @@ const DashboardAdmin = () => {
   };
 
   return (
-    <nav className="bg-red-700 p-4">
-      <div className="max-w-screen-xl flex items-center justify-between mx-auto">
-        <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src={Logo} className="h-16" alt="Logo KPU" />
-          <span className="self-center text-white text-3xl tracking-tighter font-semibold font-frank whitespace-nowrap">
-            Kota Cimahi
-          </span>
-        </Link>
-        <div className="hidden md:flex md:items-center flex-grow justify-center">
-          <ul className="font-medium text-lg flex space-x-12">
-            <li className="relative">
+    <div>
+      <nav className="bg-red-700 p-4 sticky top-0 z-50">
+        <div className="max-w-screen-xl flex items-center justify-between mx-auto">
+          <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src={Logo} className="h-16" alt="Logo KPU" />
+            <span className="self-center text-white text-3xl tracking-tighter font-semibold font-frank whitespace-nowrap">
+              Kota Cimahi
+            </span>
+          </Link>
+          <div className="hidden md:flex md:items-center flex-grow justify-center">
+            <ul className="font-medium text-lg flex space-x-12">
+              <li className="relative">
+                <button
+                  onClick={() => toggleDropdown('divisi')}
+                  className="block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-black md:p-0"
+                >
+                  Divisi
+                </button>
+                {activeDropdown === 'divisi' && (
+                  <ul className="absolute left-1/2 transform -translate-x-1/2 mt-2 bg-white text-black rounded shadow-lg w-48">
+                    <li>
+                      <Link to="/" className="block py-1 px-4 hover:bg-gray-200 rounded text-sm">Divisi Keuangan, Umum, Rumah Tangga, dan Logistik</Link>
+                    </li>
+                    <li>
+                      <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm">Divisi Teknik Penyelenggaraan</Link>
+                    </li>
+                    <li>
+                      <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm">Divisi Perencanaan, Data, & Informasi</Link>
+                    </li>
+                    <li>
+                      <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm">Divisi Hukum dan Pengawasan</Link>
+                    </li>
+                    <li>
+                      <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm">Divisi Sosialisasi, Pendidikan Pemilih, Parmas, & SDM</Link>
+                    </li>
+                  </ul>
+                )}
+              </li>
+              <li className="relative">
+                <button
+                  className="block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-black md:p-0"
+                >
+                  Sekretaris
+                </button>
+              </li>
+              <li className="relative">
+                <button
+                  onClick={() => toggleDropdown('subBagian')}
+                  className="block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-black md:p-0"
+                >
+                  Sub Bagian
+                </button>
+                {activeDropdown === 'subBagian' && (
+                  <ul className="absolute left-1/2 transform -translate-x-1/2 mt-2 bg-white text-black rounded shadow-lg w-48">
+                    <li>
+                      <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm">Sub Bagian Teknis Penyelenggaraan Pemilu, Partisipasi, & Hupmas</Link>
+                    </li>
+                    <li>
+                      <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm">Sub Bagian Perencanaan, Data & Informasi</Link>
+                    </li>
+                    <li>
+                      <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm">Sub Bagian Hukum & SDM</Link>
+                    </li>
+                    <li>
+                      <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm">Sub Bagian Keuangan, Umum, & Logistik</Link>
+                    </li>
+                  </ul>
+                )}
+              </li>
+            </ul>
+          </div>
+          <div className="hidden md:flex md:items-center">
+            <div className="relative">
               <button
-                onClick={() => toggleDropdown('divisi')}
-                className="block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-black md:p-0"
+                onClick={toggleAdminDropdown}
+                className="text-white font-medium text-lg flex items-center"
               >
-                Divisi
+                Hallo, Admin!
+                <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
+                </svg>
               </button>
-              {activeDropdown === 'divisi' && (
-                <ul className="absolute left-1/2 transform -translate-x-1/2 mt-2 bg-white text-black rounded shadow-lg w-48">
+              {adminDropdownOpen && (
+                <ul className="absolute left-1/2 transform -translate-x-1/2 mt-2 bg-white text-black rounded shadow-lg w-30">
                   <li>
-                    <Link to="/" className="block py-1 px-4 hover:bg-gray-200 rounded text-sm">Divisi Keuangan, Umum, Rumah Tangga, dan Logistik</Link>
-                  </li>
-                  <li>
-                    <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm">Divisi Teknik Penyelenggaraan</Link>
-                  </li>
-                  <li>
-                    <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm">Divisi Perencanaan, Data, & Informasi</Link>
-                  </li>
-                  <li>
-                    <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm">Divisi Hukum dan Pengawasan</Link>
-                  </li>
-                  <li>
-                    <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm">Divisi Sosialisasi, Pendidikan Pemilih, Parmas, & SDM</Link>
+                    <Link to="/" className="block px-4 py-2 w-32 hover:bg-gray-200 rounded text-center">Logout</Link>
                   </li>
                 </ul>
               )}
-            </li>
-            <li className="relative">
-              <button
-                className="block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-black md:p-0"
-              >
-                Sekretaris
-              </button>
-            </li>
-            <li className="relative">
-              <button
-                onClick={() => toggleDropdown('subBagian')}
-                className="block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-black md:p-0"
-              >
-                Sub Bagian
-              </button>
-              {activeDropdown === 'subBagian' && (
-                <ul className="absolute left-1/2 transform -translate-x-1/2 mt-2 bg-white text-black rounded shadow-lg w-48">
-                  <li>
-                    <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm">Sub Bagian Teknis Penyelenggaraan Pemilu, Partisipasi, & Hupmas</Link>
-                  </li>
-                  <li>
-                    <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm">Sub Bagian Perencanaan, Data & Informasi</Link>
-                  </li>
-                  <li>
-                    <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm">Sub Bagian Hukum & SDM</Link>
-                  </li>
-                  <li>
-                    <Link to="/" className="block py-2 px-4 hover:bg-gray-200 rounded text-sm">Sub Bagian Keuangan, Umum, & Logistik</Link>
-                  </li>
-                </ul>
-              )}
-            </li>
-          </ul>
-        </div>
-        <div className="hidden md:flex md:items-center">
-          <div className="relative">
-            <button
-              onClick={toggleAdminDropdown}
-              className="text-white font-medium text-lg flex items-center"
-            >
-              Hallo, Admin!
-              <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
+            </div>
+          </div>
+          <div className="md:hidden">
+            <button onClick={toggleMenu} className="block text-white focus:outline-none">
+              <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M4 6h16v1H4V6zm0 5h16v1H4v-1zm0 5h16v1H4v-1z"
+                />
               </svg>
             </button>
-            {adminDropdownOpen && (
-              <ul className="absolute left-1/2 transform -translate-x-1/2 mt-2 bg-white text-black rounded shadow-lg w-30">
-                <li>
-                <Link to="/" className="block px-4 py-2 w-32 hover:bg-gray-200 rounded text-center">Logout</Link>
-                </li>
-              </ul>
-            )}
           </div>
         </div>
-        <div className="md:hidden">
-          <button onClick={toggleMenu} className="block text-white focus:outline-none">
-            <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M4 6h16v1H4V6zm0 5h16v1H4v-1zm0 5h16v1H4v-1z"
-              />
-            </svg>
-          </button>
-        </div>
-      </div>
-    </nav>
+      </nav>
+
+      <section class="bg-center bg-no-repeat bg-gray-700 bg-blend-multiply" style={{ backgroundImage: `url(${BackgroundImage})` }}>
+          <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+              <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-5xl">Pengelolaan Pegawai KPU Kota Cimahi</h1>
+              <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Solusi Terbaik untuk Pengelolaan dan Pemantauan Data Pegawai KPU</p>
+              <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+                  <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                      Get started
+                      <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                      </svg>
+                  </a>
+              </div>
+          </div>
+      </section>
+    </div>
   );
 };
 
