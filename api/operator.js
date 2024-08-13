8f119ad7f87bab2d32c0de317cf50b1c5aa052fd
Ahmad Badar <ahmadbadar145@gmail.com>
2024-08-13 09:22:37 +0700

    Membuat api crud divisi pdi

--- api/operator.js
+++ api/operator.js
@@ -165,5 +165,65 @@ app.delete('/api/divisi-kurl-op/:id', (req, res) => {
     res.status(200).json({ success: true, message: 'User deleted successfully' });
   });
 });
-
 // ===================================== End endpoint buat halaman Divisi KURL =====================================
+
+
+// ===================================== Start endpoint buat halaman Divisi PDI =====================================
+// Endpoint untuk creat data anggota
+app.post('/api/divisi-pdi-op', upload.single('photo'), (req, res) => {
+  const { name, nip, position } = req.body;
+  const photo = req.file ? `/uploads/${req.file.filename}` : null;
+
+  const query = 'INSERT INTO divisi_pdi (nama_div_pdi, nip_div_pdi, posisi_div_pdi, foto_div_pdi) VALUES (?, ?, ?, ?)';
+  db.query(query, [name, nip, position, photo], (err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.status(201).json({ success: true, message: 'User added successfully' });
+  });
+});
+
+// Endpoint untuk read data anggota
+app.get('/api/divisi-pdi-op', (req, res) => {
+  const query = 'SELECT * FROM divisi_pdi';
+  db.query(query, (err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.status(200).json(results);
+  });
+});
+
+// Endpoint untuk update data anggota
+app.put('/api/divisi-pdi-op/:id', upload.single('photo'), (req, res) => {
+  const { id } = req.params;
+  const { name, nip, position } = req.body;
+  const photo = req.file ? `/uploads/${req.file.filename}` : null;
+
+  // Query untuk mendapatkan data anggota yang ada
+  const getUserQuery = 'SELECT * FROM divisi_pdi WHERE id = ?';
+  db.query(getUserQuery, [id], (err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+    if (results.length === 0) return res.status(404).json({ error: 'User not found' });
+
+    // Data yang akan diperbarui
+    const updatedName = name || results[0].nama_div_pdi;
+    const updatedNip = nip || results[0].nip_div_pdi;
+    const updatedPosition = position || results[0].posisi_div_pdi;
+    const updatedPhoto = photo || results[0].foto_div_pdi;
+
+    // Query untuk memperbarui data anggota
+    const updateUserQuery = 'UPDATE divisi_pdi SET nama_div_pdi = ?, nip_div_pdi = ?, posisi_div_pdi = ?, foto_div_pdi = ? WHERE id = ?';
+    db.query(updateUserQuery, [updatedName, updatedNip, updatedPosition, updatedPhoto, id], (err) => {
+      if (err) return res.status(500).json({ error: err.message });
+      res.status(200).json({ success: true, message: 'User updated successfully' });
+    });
+  });
+});
+
+// Endpoint untuk delete data anggota
+app.delete('/api/divisi-pdi-op/:id', (req, res) => {
+  const { id } = req.params;
+  const query = 'DELETE FROM divisi_pdi WHERE id = ?';
+  db.query(query, [id], (err) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.status(200).json({ success: true, message: 'User deleted successfully' });
+  });
+});
+// ===================================== End endpoint buat halaman Divisi PDI =====================================
