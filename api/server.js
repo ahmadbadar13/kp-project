ceb4f95362dd8dfebdae9e5da060aa4d47877087
Ahmad Badar <ahmadbadar145@gmail.com>
2024-09-06 11:06:04 +0700

    test db online

--- api/server.js
+++ api/server.js
@@ -13,20 +13,20 @@ const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.static(buildPath));
 
 // Konfigurasi koneksi database
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'kp_project'
-});
+// const db = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'kp_project'
+// });
 
 // Production database configuration
-// const db = mysql.createConnection({
-//   host: 'bz4kzerb13skvzsklu2n-mysql.services.clever-cloud.com',
-//   user: 'uteofvtkhgj76a2o',
-//   password: 'WW8UkNEM2x3438ppvsUv',
-//   database: 'bz4kzerb13skvzsklu2n'
-// });
+const db = mysql.createConnection({
+  host: 'bz4kzerb13skvzsklu2n-mysql.services.clever-cloud.com',
+  user: 'uteofvtkhgj76a2o',
+  password: 'WW8UkNEM2x3438ppvsUv',
+  database: 'bz4kzerb13skvzsklu2n'
+});
 
 db.connect((err) => {
   if (err) {
